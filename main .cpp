846c52bb8dc2b73854ab86757dbecc2a0f2c620f
TMate <toth.mate.tata@gmail.com>
2026-03-02 20:12:54 +0100

    Hibák kijavítása

--- main .cpp
+++ main .cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //nincs alulvonás a N_ELEMENTS-ben
-    std::cout << '1-100 ertekek duplazasa' // nincs pontosvessző a sor végén, " " << std::endl;
-    for (int i = 0;) // nincs feltétel a for ciklusban
+    int *b = new int[N_ELEMENTS]; //nincs alulvonás a N_ELEMENTS-ben
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // nincs pontosvessző a sor végén, " " << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++) // nincs feltétel a for ciklusban
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // nincs feltétel a for ciklusban
+    for (int i = 0; i < N_ELEMENTS; i++) // nincs feltétel a for ciklusban
     {
-        std::cout << "Ertek:" // nincs pontosvessző a sor végén, " " << b[i] << std::endl;
+        std::cout << "Ertek:" << b[i] << std::endl; // nincs pontosvessző a sor végén, " " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) // nincs pontosvessző a for ciklusban
+    int atlag = 0; // nem inicializáltuk az atlag változót
+    for (int i = 0; i < N_ELEMENTS; i++) // nincs pontosvessző a for ciklusban
     {
-        atlag += b[i] // nincs pontosvessző a sor végén
+        atlag += b[i]; // nincs pontosvessző a sor végén
     }
     atlag /= N_ELEMENTS; 
     std::cout << "Atlag: " << atlag << std::endl; 
